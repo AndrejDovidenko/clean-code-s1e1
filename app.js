8e356a83e9bbf3bbc32522a31da07fa4cef94a4d
AndrejDovidenko <andrejdovidenko@gmail.com>
2023-04-22 13:16:04 +0300

    fix(extended-1.1) add semantic tegs, edited the structure of block new-task

--- app.js
+++ app.js
@@ -33,8 +33,8 @@ var createNewTaskElement = function (taskString) {
   label.className = "task";
 
   //Each elements, needs appending
-  listItem.className = "list-el edit-mode";
-  label.className = "label-task";
+  listItem.className = "list-el";
+  label.className = "label-task task";
   checkBox.className = "input-checkbox";
   editInput.className = "input-text";
   checkBox.type = "checkbox";
@@ -82,7 +82,7 @@ var editTask = function () {
   var editInput = listItem.querySelector("input[type=text]");
   var label = listItem.querySelector("label");
   var editBtn = listItem.querySelector(".edit");
-  var containsClass = listItem.classList.contains("editMode");
+  var containsClass = listItem.classList.contains("edit-mode");
   //If class of the parent is .editmode
   if (containsClass) {
     //switch to .editmode
@@ -95,7 +95,7 @@ var editTask = function () {
   }
 
   //toggle .editmode on the parent.
-  listItem.classList.toggle("editMode");
+  listItem.classList.toggle("edit-mode");
 };
 
 //Delete task.
